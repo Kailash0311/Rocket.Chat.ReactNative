6dbe25d0fc95555fa4e4a2a2fa6a9dd431de3825
Kailash Bhalaki <kailashbhalaki@gmail.com>
2019-07-27 23:44:00 +0530

    Articles Webview: Handle Back Operation Android

--- app/views/ArticlesView/index.js
+++ app/views/ArticlesView/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { WebView } from 'react-native-webview';
+import {
+	BackHandler,
+	Platform
+} from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 import CookieManager from 'react-native-cookies';
@@ -10,6 +14,7 @@ import StatusBar from '../../containers/StatusBar';
 import { DrawerButton } from '../../containers/HeaderButton';
 
 @connect(state => ({
+	baseUrl: state.settings.Site_Url || state.server ? state.server.server : '',
 	userId: state.login.user && state.login.user.id,
 	authToken: state.login.user && state.login.user.token
 }))
@@ -20,32 +25,64 @@ export default class AdminPanelView extends React.Component {
 	});
 
 	static propTypes = {
+		baseUrl: PropTypes.string,
 		userId: PropTypes.string,
 		authToken: PropTypes.string,
 		navigation: PropTypes.object
 	}
 
+	webView = {
+		canGoBack: false,
+		ref: null
+	}
+	// [TODO] Make a back button for iOS
+
+	componentWillMount() {
+		if (Platform.OS === 'android') {
+			BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+		}
+	}
+
+	componentWillUnmount() {
+		if (Platform.OS === 'android') {
+			BackHandler.removeEventListener('hardwareBackPress');
+		}
+	}
+
+	onAndroidBackPress = () => {
+		if (this.webView.canGoBack && this.webView.ref) {
+			this.webView.ref.goBack();
+			return true;
+		}
+		return false;
+	}
+
 	render() {
-		const { navigation, authToken, userId } = this.props;
+		const {
+			navigation, authToken, userId, baseUrl
+		} = this.props;
 		this.articlesLink = navigation.getParam('articlesLink');
-		console.warn('authToken is', authToken, 'userID is', userId);
 		if (!this.articlesLink) {
 			return null;
 		}
-		// CookieManager.setFromResponse(
-		// 	this.articlesLink,
-		// 	`rc_uid=${ userId };rc_token=${ authToken };`
-		// )
+		// CookieManager.get(baseUrl)
 		// 	.then((res) => {
-		// 		// `res` will be true or false depending on success.
-		// 		alert('cookies set');
+		// 		console.warn('CookieManager.get =>', res);
+		// 		/* [TODO]
+		// 		* Based on whether baseUrl has cookies required for the articlesLink,
+		// 		* in this case, rc_uid and rc_token,
+		// 		* change injectedJavaScript such that
+		// 		*	no-login appears if cookies are already present.
+		// 		*/
 		// 	});
 		return (
 			<SafeAreaView style={styles.container} testID='articles-view'>
 				<StatusBar />
 				<WebView
-					source={{ uri: this.articlesLink }}
-					// injectedJavaScript={`document.cookie='rc_token=${ authToken }; rc_uid=${ userId };'`}
+					source={{ uri: `${ baseUrl }/admin/info?layout=embedded` }}
+					ref={(webView) => { this.webView.ref = webView; }}
+					injectedJavaScript={`Meteor.loginWithToken('${ authToken }', function() { }); setTimeout(function(){ window.location.assign('${ this.articlesLink }')}, 50)`}
+					onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
 				/>
 			</SafeAreaView>
 		);
